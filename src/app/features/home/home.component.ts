f81e7b6dbcec738d06933f6070e7b29b649dfa49
Gerardo3Carcamo <gcarcamo@uadec.edu.mx>
2025-11-24 10:42:25 -0600

    home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CardModule } from 'primeng/card'
+import { ButtonModule } from 'primeng/button'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    CommonModule, ButtonModule, RouterModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
