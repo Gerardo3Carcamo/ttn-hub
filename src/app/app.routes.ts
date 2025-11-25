80ab282db93aa352aee19e54b0d7918c1720089d
Gerardo3Carcamo <gcarcamo@uadec.edu.mx>
2025-11-24 23:19:16 -0600

    feat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,9 @@ export const routes: Routes = [
                 component: HomeComponent
             }
         ]
+    },
+    {
+        path: 'auth',
+        component: LoginComponent
     }
 ];
--- src/app/shared/components/topbar/topbar.component.ts
+++ src/app/shared/components/topbar/topbar.component.ts
@@ -1,14 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NavigateService } from '../../services/navigate.service';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
   imports: [],
+  providers: [NavigateService],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
 
+  constructor(private navigate: NavigateService){}
+
   @Output() onMenuClick: EventEmitter<void> = new EventEmitter<void>();
+
+  doLogin(){
+    this.navigate.navigate('/auth');
+  }
 
 }
